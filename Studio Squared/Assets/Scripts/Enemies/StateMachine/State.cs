b35417614eadea28723baf7ad93c9b8386081b38
NotALobster <92136499+NotALobster@users.noreply.github.com>
2023-07-16 16:25:51 -0700

    Starting better state machine

---
+++ Studio Squared/Assets/Scripts/Enemies/StateMachine/State.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    protected FiniteStateMachine stateMachine;
+    protected StateEntity entity;
+    
+    protected float startTime;
+
+
+    public State(StateEntity entity, FiniteStateMachine stateMachine){
+        this.entity = entity;
+        this.stateMachine = stateMachine;
+    }
+
+    //called on state enter
+    public virtual void Enter(){
+        startTime = Time.time;
+    }
+
+    //called on state exit
+    public virtual void Exit(){
+
+    }
+
+    //called on Update()
+    public virtual void LogicUpdate(){
+        
+    }
+
+    //called on FixedUpdate()
+    public virtual void PhysicsUpdate(){
+
+    }
+
+
+
+}
---
+++ Studio Squared/Assets/Scripts/Enemies/StateMachine/StateEntity.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateEntity : MobileEntity
+{
+
+    public new virtual void Start(){
+        base.Start()
+    }
+}
