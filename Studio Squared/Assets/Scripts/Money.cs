a9f678ab08b1bc549d80e1e258e158800c0a324a
lzhou0714 <lzhou0714@g.ucla.edu>
2023-09-20 18:26:36 -0700

    coin spawn system

--- Studio Squared/Assets/Scripts/Money.cs
+++ Studio Squared/Assets/Scripts/Money.cs
@@ -3,16 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Money : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]private int value;
-    [SerializeField] private TMP_Text playerCoins;
-    
-    void Start()
+    [SerializeField] private TextMeshProUGUI playerCoins;
+    private Vector2 off;
+    [SerializeField] private GameObject parent;
+    [SerializeField]private int moveDuration = 1;
+    private float delay = 0;
+    void Awake()
     {
+        off = new Vector2(Random.Range(-3f, 3f), Random.Range(0f, 3f));
+        parent.GetComponent<Rigidbody2D>().velocity = off;
+        playerCoins = GameObject.Find("num_coins").GetComponent<TextMeshProUGUI>();
+    }
+    
+    private void FixedUpdate()
+    {
+        transform.position = parent.transform.position;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,9 +32,10 @@ public class Money : MonoBehaviour
         
         if (other.CompareTag("Player"))
         {
-
+            Debug.Log("destroy");
+            Destroy(parent);
             playerCoins.text = Player.self.updateCoins(value).ToString();
-            Destroy(gameObject);
+            
         }
         
     }
--- Studio Squared/Assets/Scripts/currencySystem.cs
+++ Studio Squared/Assets/Scripts/currencySystem.cs
@@ -1,5 +1,8 @@
+using System;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CurrencySystem : MonoBehaviour
 {
@@ -7,36 +10,51 @@ public class CurrencySystem : MonoBehaviour
     [SerializeField] private Money fives;
     [SerializeField] private Money tens;
     [SerializeField] private Money fifties;
+    [SerializeField] private int amountSpawn;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SpawnCoins(amountSpawn);
+        }
+    }
+
     public void SpawnCoins(int amount)
     {
         int numOnes = amount < 10 ? amount : 10;
         for (int i = 0; i < numOnes; i++)
         {
-            Instantiate(ones);
+            float offset = Random.Range(-0.1f, 0.5f);
+
+            Instantiate(ones, new Vector2(transform.position.x + offset, transform.position.y), quaternion.identity);
         }
 
         amount -= 10;
         while (amount > 0)
         {
+            float offset = Random.Range(-0.5f, 0.5f);
+            Debug.Log(amount);
+            Debug.Log(offset);
             if (amount < 5)
             {
-                Instantiate(ones);
+                Instantiate(ones, new Vector2(transform.position.x + offset, transform.position.y), quaternion.identity);
                 amount--;
             }
 
             else if (amount < 10)
             {
-                Instantiate(fives);
+                Instantiate(fives,new Vector2(transform.position.x + offset, transform.position.y), quaternion.identity);
                 amount -= 5;
             }
             else if (amount <20)
             {
-                Instantiate(tens);
+                Instantiate(tens,new Vector2(transform.position.x + offset, transform.position.y), quaternion.identity);
                 amount -= 10;
             }
             else if (amount > 50)
             {
-                Instantiate(fifties);
+                Instantiate(fifties,new Vector2(transform.position.x + offset, transform.position.y), quaternion.identity);
                 amount -= 50;
             }
         }
